fbdbd0a6eea02f98ae238021c3a32153a379c8eb
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-07-17 15:20:48 +0300

    done unimodal array

--- cs161_stanford/src/main/java/algo1/UnimodalArray.java
+++ cs161_stanford/src/main/java/algo1/UnimodalArray.java
@@ -1,7 +1,73 @@
 package algo1;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by ilyarudyak on 7/17/16.
+ * 2. You are a given a unimodal array of n distinct elements,
+ * meaning that its entries are in increasing order up until
+ * its maximum element, after which its elements are in
+ * decreasing order. Give an algorithm to compute the maximum
+ * element that runs in O(log n) time.
  */
 public class UnimodalArray {
+
+    private List<Integer> list;
+
+    public UnimodalArray(List<Integer> list) {
+        this.list = list;
+    }
+
+    public int max() {
+        return max(0, list.size() - 1);
+    }
+
+    // helper methods
+    private int max(int i, int j) {
+
+        if (j - i == 1) {
+            return Math.max(list.get(i), list.get(j));
+        }
+
+        // med == 0 only when i == 0 and j == 1 but
+        // this is the case above
+        int med = (i + j) / 2;
+
+        if (list.get(med - 1) < list.get(med)) {
+            // go right
+            return max(med, j);
+        } else {
+            // go left
+            return max(i, med);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        UnimodalArray ua = new UnimodalArray(Arrays.asList(3, 5, 7, 6, 4, 2));
+        System.out.println(ua.max());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cs161_stanford/src/test/java/algo1/UnimodalArrayTest.java
@@ -0,0 +1,71 @@
+package algo1;
+
+import org.junit.Before;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ilyarudyak on 7/17/16.
+ */
+public class UnimodalArrayTest {
+
+    private static final int SIZE = 10;
+    private static final int ITERATION = 100;
+    private static final int BASE = 1000;
+    private List<List<Integer>> lists;
+    private Random random = new Random(new Date().getTime());
+    private List<UnimodalArray> arrays;
+
+    @Before
+    public void setUp() throws Exception {
+        lists = new ArrayList<>();
+        arrays = new ArrayList<>();
+        for (int i = 0; i < ITERATION; i++) {
+            List<Integer> l = buildUnimodalList();
+            lists.add(l);
+            arrays.add(new UnimodalArray(l));
+        }
+    }
+
+    private List<Integer> buildUnimodalList() {
+        List<Integer> list = new ArrayList<>();
+
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+
+        int size = random.nextInt(SIZE);
+        for (int i = 0; i < size; i++) {
+            l1.add(random.nextInt(BASE));
+        }
+        for (int i = 0; i < SIZE - size - 1; i++) {
+            l2.add(random.nextInt(BASE));
+        }
+
+        int max1 = 0, max2 = 0;
+        if (!l1.isEmpty()) {
+            max1 = Collections.max(l1);
+        }
+        if (!l2.isEmpty()) {
+            max2 = Collections.max(l2);
+        }
+
+        Collections.sort(l1);
+        Collections.sort(l2, Collections.<Integer>reverseOrder());
+
+        list.addAll(l1);
+        list.add(max1 + max2);
+        list.addAll(l2);
+
+//        System.out.println(list);
+        return list;
+    }
+
+    @org.junit.Test
+    public void testMax() throws Exception {
+        for (int i = 0; i < ITERATION; i++) {
+            assertEquals((int)Collections.max(lists.get(i)), arrays.get(i).max());
+        }
+    }
+}
