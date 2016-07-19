b2760b8bc164ae2e9eb8f302dfd5314ba8c21a1b
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-07-19 23:28:29 +0300

    add merge sort from Princeton

---
+++ cs161_stanford/src/main/java/algo1/pas/pa1/Inversion.java
@@ -0,0 +1,59 @@
+package algo1.pas.pa1;
+
+import java.util.Arrays;
+
+/**
+ * Created by ilyarudyak on 7/19/16.
+ */
+public class Inversion {
+
+    private int[] a;
+    private int[] aux;
+
+    public Inversion(int[] a) {
+        this.a = a;
+        aux = new int[a.length];
+    }
+
+    private void merge(int lo, int mid, int hi) {
+
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        // merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)               a[k] = aux[j++];
+            else if (j > hi)                a[k] = aux[i++];
+            else if (aux[j] < aux[i])       a[k] = aux[j++];
+            else                            a[k] = aux[i++];
+        }
+    }
+
+    // merge sort a[lo..hi] using auxiliary array aux[lo..hi]
+    private void sort(int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(lo, mid);
+        sort(mid + 1, hi);
+        merge(lo, mid, hi);
+    }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     */
+    public void sort() {
+        sort(0, a.length-1);
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {5, 4, 3, 2, 1};
+        Inversion inversion = new Inversion(a);
+        inversion.sort();
+        System.out.println(Arrays.toString(inversion.a));
+
+    }
+}
